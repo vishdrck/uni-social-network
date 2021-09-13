dc661594a974c136087749077a12f407ade1aabe
Vishwajith <vish.drck@gmail.com>
2021-09-13 13:16:12 +0530

    Added SVG component

--- src/app/Elements/svg-icon/svg-icon.component.ts
+++ src/app/Elements/svg-icon/svg-icon.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-svg-icon',
+  selector: 'svg-icon',
   templateUrl: './svg-icon.component.html',
   styleUrls: ['./svg-icon.component.scss']
 })
 export class SvgIconComponent implements OnInit {
-
-  constructor() { }
+  @Input() icon = '';
+  svgContent: string;
+  customParentCss: string;
+  customSVGCss: string;
+  svgPath = '/assets/svg/icons';
+  constructor() {
+    this.svgContent = '';
+    this.customParentCss = '';
+    this.customSVGCss = '';
+  }
 
   ngOnInit(): void {
+    this.svgContent = `<img class="mat-icon notranslate mat-icon-no-color ${this.customSVGCss}" src=${this.svgPath}/${this.icon}.svg>`;
+
   }
 
 }
