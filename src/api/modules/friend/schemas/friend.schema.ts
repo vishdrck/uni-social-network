0eeb0458b979850789d94b2ffab97f4765af91f8
Vishwajith <vish.drck@gmail.com>
2021-09-26 17:10:42 +0530

    Added friend schema

---
+++ src/api/modules/friend/schemas/friend.schema.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const SCHEMA = mongoose.Schema;
+
+const updateNote = {
+  updatedOn: Date,
+  updatedBy: String,
+  updateNote: String
+};
+
+const schema = new SCHEMA({
+  _uid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users'
+  },
+  _fuid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users'
+  },
+  requestedDate: {
+    type: Date
+  }
+});
+
+export default mongoose.model('Friends', schema,'friends');
