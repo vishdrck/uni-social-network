56bc4dcd752c8118c2b078612b35488933580cf4
Vishwajith <vish.drck@gmail.com>
2021-10-08 21:41:08 +0530

    Changed the environments in both App and API

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -1,6 +1,22 @@
+enum Environments {
+  LOCAL_WINDOWS = 'win',
+  DEV_SERVER = 'dev',
+  QA_SERVER = 'qa',
+  PROD_SERVER = 'prod'
+}
 class ConstantsService {
+  private readonly env: Environments;
+  private readonly port = 2521;
+  private readonly host = 'https://www.yaatter.xyz/api/';
+  constructor(env: Environments) {
+    this.env = env;
+  }
   public getAPIUrl(): string {
-    return 'http://localhost:2521/';
+    let url = `http://localhost:${this.port}/`;
+    if(this.env === Environments.PROD_SERVER) {
+      url = this.host;
+    }
+    return url;
   }
 
   public getURL(endpoint: string | null = null): string {
@@ -12,4 +28,4 @@ class ConstantsService {
   }
 }
 
-export default new ConstantsService();
+export default new ConstantsService(Environments.PROD_SERVER);
